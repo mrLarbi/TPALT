8c3dc35ae07d44c1023e053520ff81006d91d9ac
mrLarbi <larbiyoucef.mohamed@gmail.com>
2015-10-27 12:00:22 +0100

    Password validation fixed, I'm dumb, I a forgot that we already have regexp in the server :)

--- app/src/main/java/treasurehunt/mobile/LoginActivity.java
+++ app/src/main/java/treasurehunt/mobile/LoginActivity.java
@@ -211,7 +211,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isEmailValid(String email) {
-        String emailRegExp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+        String emailRegExp = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
         return email.matches(emailRegExp);
     }
 
@@ -224,11 +224,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * 2 if it is not valid because it does not match the regular expression.
      */
     private int isPasswordValid(String password) {
-        String passwordRegExp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\\\S+$).{4,}$";
+        String passwordRegExp = "^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$";
         if(password.length() <= 6) {
             return 1;
         }
-        if(password.matches(passwordRegExp)) {
+        if(!password.matches(passwordRegExp)) {
             return 2;
         }
         return  0;
