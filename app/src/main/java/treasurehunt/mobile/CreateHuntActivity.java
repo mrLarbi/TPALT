8b6ace1bc5e7313c8818a24d46d1bcc844ecad8d
juanmanuel-calderon <juanmanuelcalderon812@gmail.com>
2015-10-31 16:27:06 +0100

    small layout changes

--- app/src/main/java/treasurehunt/mobile/CreateHuntActivity.java
+++ app/src/main/java/treasurehunt/mobile/CreateHuntActivity.java
@@ -1,20 +1,10 @@
 package treasurehunt.mobile;
 
 import android.app.Activity;
-import android.content.DialogInterface;
-import android.os.PersistableBundle;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class CreateHuntActivity extends Activity implements View.OnClickListener {
 
